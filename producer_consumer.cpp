35087596241deed46266493f4e3e5cebfa497c1d
codeBehindMe <aaron.tillekeratne@gmail.com>
2023-06-05 10:52:16 +0000

    Completed stack

--- producer_consumer.cpp
+++ producer_consumer.cpp
@@ -58,8 +58,90 @@ private:
   const unsigned int _size = 10; // Max size of our queue
 };
 
+class Producer
+{
+public:
+  Producer(Buffer *buffer, std::string name)
+  {
+    this->_buffer = buffer;
+    this->_name = name;
+  }
+
+  void start()
+  {
+    while (true)
+    {
+      // Create a random work (int) to add in to the queue.
+      int num = std::rand() % 100;
+      this->_buffer->add(num);
+      int sleep_time = rand() % 100; // Sleep time to sleep before producer creates more work
+
+      cout_mu.lock(); // Aquire cout mutex so multiple producers are not writing to cout at the same time.
+      std::cout << "Producer: " << this->_name << " added work: " << num << " Sleeping for: " << sleep_time << std::endl;
+      std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
+      cout_mu.unlock();
+    }
+  }
+
+private:
+  Buffer *_buffer;
+  std::string _name;
+};
+
+class Consumer
+{
+
+public:
+  Consumer(Buffer *buffer, std::string name)
+  {
+    this->_buffer = buffer;
+    this->_name = name;
+  }
+
+  void start()
+  {
+    while (true)
+    {
+      // Takle work (int) off the buffer and just print.
+      int num = this->_buffer->take();
+      int sleep_time = std::rand() % 100;
+
+      cout_mu.lock();
+      std::cout << "Consumer: " << this->_name << " took work: " << num << " sleeping for: " << sleep_time << std::endl;
+      std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
+      cout_mu.unlock();
+    }
+  }
+
+private:
+  Buffer *_buffer;
+  std::string _name;
+};
+
 int main()
 {
 
+  Buffer b;
+
+  Producer p1(&b, "producer1");
+  Producer p2(&b, "producer2");
+
+  Consumer c1(&b, "consumer1");
+  Consumer c2(&b, "consumer2");
+
+  // Start producers and consumers on their own threads.
+  std::thread producer1_thread(&Producer::start, &p1);
+  std::thread producer2_thread(&Producer::start, &p2);
+
+  std::thread consumer1_thread(&Consumer::start, &c1);
+  std::thread consumer2_thread(&Consumer::start, &c2);
+
+  producer1_thread.join();
+  producer2_thread.join();
+
+  consumer1_thread.join();
+  consumer2_thread.join();
+
+  std::getchar();
   return 0;
 };
